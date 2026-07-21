42f91ede6d0094a68fdf79429734045f44badb8f
Elmaron <info@vindona.de>
2026-07-21 14:08:58 +0200

    Structural Update
    * Added InternalDifficulty
    * Added InternalDifficulty to Task

--- Tasker/Classes/Data/Conversion/Tables/Task.cs
+++ Tasker/Classes/Data/Conversion/Tables/Task.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
+using Tasker.Classes.Data.Retrieval;
 using Tasker.Classes.Templates;
 
 namespace Tasker.Classes.Data.Conversion.Tables
@@ -8,12 +11,25 @@ namespace Tasker.Classes.Data.Conversion.Tables
     public class Task : ObservableObjectTemplate
     {
         private InternalPriority _selectedPriority;
-        public InternalPriority SelectedPriority { get => _selectedPriority; set => _selectedPriority = value; }
+        private InternalDifficulty? _selectedDifficulty;
+        public InternalPriority SelectedPriority { get => _selectedPriority; set => Update(ref _selectedPriority, value); }
+        public InternalDifficulty? SelectedDifficulty { get => _selectedDifficulty; set => Update(ref _selectedDifficulty, value); }
 
         public Task(int pId, InternalData pData, InternalPriority pSelectedPriority) : base(pId, pData)
         {
             _selectedPriority = pSelectedPriority;
             System.Diagnostics.Debug.WriteLine($"Class -Appointment-; Found: {Data.Label}");
+        }
+
+        private void Update<T>(ref T field, T value, [CallerMemberName] string fieldName = "")
+        {
+            field = value;
+            new DataBase.Task().Update(new DataBase.Task.TaskData
+            {
+                Id = Id,
+                PriorityId = _selectedPriority.Id,
+                DifficultyId = _selectedDifficulty.Value.Id
+            });
         }
     }
 }
